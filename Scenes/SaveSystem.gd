fb05a586239f0945b52bcfd0e3c76ac18428d371
emmcalla <59890244+emmcalla@users.noreply.github.com>
2020-02-26 11:50:13 -0500

    Project Complete
    Project Complete

--- Scenes/SaveSystem.gd
+++ Scenes/SaveSystem.gd
@@ -2,11 +2,16 @@ extends Node2D
 
 var DisplayValue = 0
 
-func _ready():
-	pass 
+var save_path = "res://save-file.cfg"
+var config = ConfigFile.new()
+var load_response = config.load(save_path)
 
-func saveValue():
+func _ready():
 	pass
-	
-func loadValue():
-	pass
+
+func saveValue(section, key):
+	config.set_value(section, key, DisplayValue)
+	config.save(save_path)
+
+func loadValue (section, key):
+	DisplayValue = config.get_value(section, key, DisplayValue)
